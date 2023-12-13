b3cb418c9f8329fc7a5baaa30a168d02010e08a5
PossessedCoder <dmitrijnikolae67@gmail.com>
2023-12-13 21:55:23 +0300

    Editor; Block class

--- game.py
+++ game.py
@@ -1,4 +1,7 @@
+import pygame
+
 from mixins import ArrangementMixin
+from pygame import Rect
 
 
 class Cell(ArrangementMixin):
@@ -6,3 +9,15 @@ class Cell(ArrangementMixin):
     def __init__(self, x, y, w, h, image):
         self.image = image
         super().__init__(x, y, w, h)
+
+    def get_rect(self):
+        return pygame.Rect(self._x, self._y, self._w, self._h)
+
+
+class Block(Cell):
+    def __init__(self, x, y, w, h, image):
+        super().__init__(x, y, w, h, image)
+
+    def collision(self, other):
+        # type(other) == pygame.Rect
+        return Rect.colliderect(self.get_rect(), other)
