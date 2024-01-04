724980a55d5aa6b2635a40c785f0c804b76fdde2
PossessedCoder <dmitrijnikolae67@gmail.com>
2024-01-04 15:01:45 +0300

    Editor update

--- source/tiles.py
+++ source/tiles.py
@@ -3,10 +3,10 @@ import pygame
 from abstractions import AbstractSurface
 from game import Cell
 from utils import load_image
+from math import sin, cos, radians
 
 
 class Hero(Cell):
-
     class _ArrowVector(AbstractSurface):  # Do not inherit from cell. Field.add_cells() will cause issues
 
         def __init__(self, x, y, w, h, parent=None):
@@ -24,7 +24,12 @@ class Hero(Cell):
             self._image = pygame.transform.scale(loaded, self.get_size())
             self._original_image = self._image.copy()
 
-        def rotate(self, angle):
+        def rotate_pivot(self, angle, pivot: tuple):
+            self.rotate_center(angle)
+            self.move(pivot[0] - cos(radians(angle)) * self.get_rect().centerx,
+                      pivot[1] - sin(radians(angle)) * self.get_rect().centery)
+
+        def rotate_center(self, angle):
             self._angle = (self._angle + angle) % 360  # Value will repeat after 359. This prevents angle to overflow.
             self._image = pygame.transform.rotate(self._original_image, self._angle)
             self.resize(*self._image.get_size())
@@ -46,13 +51,14 @@ class Hero(Cell):
         return pygame.Rect(self.get_rect().x, self.get_rect().y - self.get_height(), *self.get_size())
 
     def update(self, *events) -> None:
-        self.move(self.get_rect().x + 1, self.get_rect().y + 1)
-        self._arrow_vector.move(*self._get_arrow_vector_rect().topleft)
+        # self.move(self.get_rect().x + 1, self.get_rect().y + 1)
+        self._arrow_vector.move(self._get_arrow_vector_rect().x, self._get_arrow_vector_rect().y)
+        ...
 
     def draw(self):
         super().draw()
 
         if self._arrowed:
-            self._arrow_vector.rotate(1.5)
+            # self._arrow_vector.rotate_pivot(1.5, self.get_rect().center)
             self._arrow_vector.draw()
             self._field.blit(self._arrow_vector)
