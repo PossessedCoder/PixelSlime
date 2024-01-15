b2586212410ce7726e1c3268f590e52835dd493f
DouleLove <sashazhur108@gmail.com>
2024-01-15 22:14:53 +0300

    refactor: rename constants.Images to constants.Media

--- source/levels.py
+++ source/levels.py
@@ -1,5 +1,5 @@
-from constants import Images
-from templates import BaseWindow, Button, BaseSurface, Form, FormField
+from constants import Media
+from templates import BaseWindow, Button
 from utils import load_media
 
 
@@ -10,24 +10,24 @@ class Levels(BaseWindow):
         self._but_w, self._but_h = 100, 100
         self._buttons = []
         self.back_button = Button(0, 0, self._but_w, self._but_h)
-        self.back_button.set_hovered_view(load_media(Images.BACK), background_color=(69, 69, 69),
+        self.back_button.set_hovered_view(load_media(Media.BACK), background_color=(69, 69, 69),
                                           border_radius=15, scale_x=1.03, scale_y=1.03)
-        self.back_button.set_not_hovered_view(load_media(Images.BACK), background_color=(78, 78, 78),
+        self.back_button.set_not_hovered_view(load_media(Media.BACK), background_color=(78, 78, 78),
                                               border_radius=25)
         self.user_levels_list_button = Button(self.get_width() - self._but_w * 3, self.get_height() - self._but_h,
                                               self._but_w * 3, self._but_h)
-        self.user_levels_list_button.set_hovered_view(load_media(Images.BACK), background_color=(69, 69, 69),
+        self.user_levels_list_button.set_hovered_view(load_media(Media.BACK), background_color=(69, 69, 69),
                                                       border_radius=15, scale_x=1.03, scale_y=1.03)
-        self.user_levels_list_button.set_not_hovered_view(load_media(Images.BACK), background_color=(78, 78, 78),
+        self.user_levels_list_button.set_not_hovered_view(load_media(Media.BACK), background_color=(78, 78, 78),
                                                           border_radius=25)
 
         for x in range(self._but_w, self.get_width() - self._but_w, self._but_w + self._but_w // 2):
             for y in range(self._but_h, self.get_height() - self._but_h,
                            self._but_h + self._but_h // 2):
                 a = Button(x, y, self._but_w, self._but_h, parent=self)
-                a.set_hovered_view(load_media(Images.LEVELS_PREVIEW), background_color=(69, 69, 69),
+                a.set_hovered_view(load_media(Media.LEVELS_PREVIEW), background_color=(69, 69, 69),
                                    border_radius=15, scale_x=1.03, scale_y=1.03)
-                a.set_not_hovered_view(load_media(Images.LEVELS_PREVIEW), background_color=(78, 78, 78),
+                a.set_not_hovered_view(load_media(Media.LEVELS_PREVIEW), background_color=(78, 78, 78),
                                        border_radius=25)
                 self._buttons.append(a)
 
