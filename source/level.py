666118e21fecf81b6a5d71701d7d68137ce9ca6e
DouleLove <sashazhur108@gmail.com>
2024-01-18 20:33:19 +0300

    implement save, remove, opening of new tiles

--- source/level.py
+++ source/level.py
@@ -1,116 +1,67 @@
-from queue import Queue
+from datetime import datetime
 
 import pygame
 
-from game import Field
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH, Media, UserEvents
-from templates import Panel, BaseWindow, LowerPanel, Button
-from utils import get_tiles, load_media, post_event
-
-
-class NotificationsPanel(Panel):
-
-    def __init__(self, minimized_rect, maximized_rect, resize_time, parent=None):
-        super().__init__(minimized_rect, maximized_rect, resize_time, parent=parent)
-
-        self._text_surface = pygame.Surface(self._maximized_rect.size, pygame.SRCALPHA)
-        self._queue = Queue()
-
-        self._current_notification = (pygame.Surface((0, 0)), pygame.Surface((0, 0)), pygame.Surface((0, 0)))
-
-        # for tile in get_tiles().values():
-        #    self.add_notification('Вы открыли новый тайл', 'Доступен в редакторе уровней', load_image(tile.IMAGE_NAME))
-
-    @property
-    def current_title(self):
-        return self._current_notification[0]
-
-    @property
-    def current_text(self):
-        return self._current_notification[1]
-
-    @property
-    def current_image(self):
-        return self._current_notification[2]
-
-    def add_notification(self, title=..., text=..., image=...):
-        title = pygame.font.SysFont('serif', 18).render(title if isinstance(title, str) else '', True, (0, 0, 0))
-        text = pygame.font.SysFont(
-            'arial', 11, italic=True).render(text if isinstance(text, str) else '', True, (69, 69, 69))
-        image = pygame.transform.scale(image.copy(), (self.get_height() // 2, self.get_height() // 2)) \
-            if isinstance(image, pygame.Surface) else pygame.Surface((0, 0))
-        self._queue.put((title, text, image))
-
-    def handle(self):
-        if self.is_minimized():
-            try:
-                self._current_notification = next(self)
-                self.maximize(duration=3)
-            except StopIteration:
-                return
-
-        super().handle()
-
-        self.fill((204, 191, 190))
-        self._text_surface.fill((255, 255, 255, 0))
-
-        self._text_surface.blit(
-            self.current_title,
-            ((self._text_surface.get_width() - self.current_title.get_width() + self.current_image.get_width()) // 2,
-             (self._text_surface.get_height() - self.current_title.get_height() - self.current_text.get_height()) // 2)
-        )
-        self._text_surface.blit(
-            self.current_text,
-            ((self._text_surface.get_width() - self.current_text.get_width() + self.current_image.get_width()) // 2,
-             (self._text_surface.get_height() - self.current_text.get_height() + self.current_title.get_height()) // 2)
-        )
-        self._text_surface.blit(
-            self.current_image,
-            (self.current_image.get_width() // 4, self.current_image.get_height() // 2)
-        )
-        self.blit(
-            self._text_surface,
-            pygame.Rect(self.get_rect().w - self.get_width(), self.get_rect().h - self.get_height(), *self.get_size())
-        )
-
-    def __next__(self):
-        if self._queue.empty():
-            raise StopIteration('Notifications queue is empty') from None
-        return self._queue.get()
+from game import Field, Coordinates
+from templates import NotificationsPanel, BaseWindow, LowerPanel, Button
+from tiles import Hero
+from utils import load_media, post_event, DataBase, get_tiles, catch_events
 
 
 class StartPanel(LowerPanel):
 
-    def __init__(self, minimized_rect, maximized_rect, resize_time=0.0, parent=None):
+    def __init__(self, is_author, minimized_rect, maximized_rect, resize_time=0.0, parent=None):
         super().__init__(minimized_rect, maximized_rect, resize_time, parent=parent)
         if self._maximized_rect.collidepoint(pygame.mouse.get_pos()):
             self.move(*self._maximized_rect.topleft)
 
-        buttons_not_hovered_view = {'scale_x': 1, 'scale_y': 1, 'border_radius': 14}
-        buttons_hovered_view = {'scale_x': 1.05, 'scale_y': 1.05, 'border_radius': 11}
-        buttons_data = (
-            (Media.CLOSE_WINDOW, (lambda: post_event(UserEvents.CLOSE_CWW),)),
-        )
+        buttons_not_hovered_view = {'scale_x': 1, 'scale_y': 1, 'border_radius': 24}
+        buttons_hovered_view = {'scale_x': 1.05, 'scale_y': 1.05, 'border_radius': 21}
+        buttons_data = [
+            (Media.CLEAR, (self.parent.field_to_initial, self.parent.restart)),
+            (Media.CLOSE_WINDOW, (lambda: post_event(UserEvents.CLOSE_CWW),))
+        ]
+        if is_author:
+            buttons_data.append((Media.TRASH_BIN, (self.parent.remove,)))
 
         for image_name, callbacks in buttons_data:
-            btn = Button(-1, -1, 50, 50, parent=self)
+            btn = Button(-1, -1, 100, 100, parent=self)
             buttons_hovered_view['content'] = load_media(image_name)
             buttons_not_hovered_view['content'] = load_media(image_name)
-            btn.set_hovered_view(**buttons_hovered_view)
-            btn.set_not_hovered_view(**buttons_not_hovered_view)
+            btn.set_hovered_view(**buttons_hovered_view, background_color=(102, 121, 213))
+            btn.set_not_hovered_view(**buttons_not_hovered_view, background_color=(85, 106, 208))
             btn.bind_press(*callbacks)
             self.add_button(btn)
 
 
 class Level(BaseWindow):
 
-    def __init__(self, data):
+    def __init__(self, level_id, uid, *, _d=None):
         super().__init__()
 
+        self._level_id = level_id
+        self._uid = uid
+
         self._start_panel = StartPanel(
+            DataBase().get_level_by_id(level_id)[2] == uid,
             (0, SCREEN_HEIGHT // 18 * 17, SCREEN_WIDTH, SCREEN_HEIGHT // 6),
             (0, SCREEN_HEIGHT // 6 * 5, SCREEN_WIDTH, SCREEN_HEIGHT // 6),
             resize_time=0.3,
+            parent=self
+        )
+
+        self._notifications_panel = NotificationsPanel(
+            (SCREEN_WIDTH, SCREEN_HEIGHT // 2 - SCREEN_HEIGHT // 4, SCREEN_WIDTH // 6, SCREEN_HEIGHT // 8),
+            (SCREEN_WIDTH // 6 * 5, SCREEN_HEIGHT // 2 - SCREEN_HEIGHT // 4, SCREEN_WIDTH // 6, SCREEN_HEIGHT // 8),
+            resize_time=0.5,
+            parent=self
+        )
+
+        self._notifications_panel2 = NotificationsPanel(
+            (SCREEN_WIDTH, SCREEN_HEIGHT // 2 - SCREEN_HEIGHT // 10, SCREEN_WIDTH // 6, SCREEN_HEIGHT // 8),
+            (SCREEN_WIDTH // 6 * 5, SCREEN_HEIGHT // 2 - SCREEN_HEIGHT // 10, SCREEN_WIDTH // 6, SCREEN_HEIGHT // 8),
+            resize_time=0.5,
             parent=self
         )
 
@@ -123,22 +74,93 @@ class Level(BaseWindow):
         self._field.rows, self._field.cols = 15, 15
         self._field.grid = (255, 255, 255)
 
-        self._setup_field(data)
+        self._setup_field(DataBase().get_level_field_data(level_id) if _d is None else _d)
 
-    # data: dict[str, str | Type[Cell]]
+        self._best_time = None
+        self._start_time = None
+
+        self._wait_until_invoked = False
+
+        self.restart()
+
+    def remove(self):
+        DataBase().delete_level(self._level_id)
+        post_event(UserEvents.CLOSE_CWW)
+
+    def field_to_initial(self):
+        for cl in self._field.get_cells():
+            cl.to_initial()
+
+    def restart(self):
+        self._notifications_panel.add_notification('Нажмите любую кнопку', image=load_media(Media.CLOCK))
+        self._start_time = None
+        self._field.handle()
+        self._wait_until_invoked = True
+
+    @classmethod
+    def from_data(cls, data):
+        return cls(-1, -1, _d=data)
+
     def _setup_field(self, data):
-        g = get_tiles()
         cells = []
 
         for coordinates, factory in data:
+            coordinates = Coordinates(*map(int, coordinates.split())) if isinstance(coordinates, str) else coordinates
+            factory = get_tiles()[factory] if isinstance(factory, str) else factory
             cells.append(factory(self._field, coordinates))
 
         self._field.add_cells(*cells)
 
+    def eventloop(self):
+        if not self._wait_until_invoked:
+            self._field.handle()
+            if self._notifications_panel.is_maximized():
+                self._notifications_panel.minimize()
+        for event in catch_events(False):
+            if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                self._wait_until_invoked = False
+                # clearing queue of notifications panel since multiple waiter notifications might be added
+                while True:
+                    try:
+                        next(self._notifications_panel)
+                    except StopIteration:
+                        return
+
     def draw(self):
         self.fill((54, 57, 62))
 
-        self._field.handle()
         self.blit(self._field)
         self._start_panel.handle()
         self.blit(self._start_panel)
+        self._notifications_panel.handle()
+        if not self._notifications_panel.is_minimized():
+            self.blit(self._notifications_panel)
+        self._notifications_panel2.handle()
+        if not self._notifications_panel2.is_minimized():
+            self.blit(self._notifications_panel2)
+
+    def handle(self):
+        self.eventloop()
+        self.draw()
+
+        if self._start_time is None:
+            self._start_time = datetime.now()
+
+        if (hero := next(filter(lambda x: isinstance(x, Hero), self._field.get_cells()), None)) is None:
+            return
+
+        if hero.finished:
+            for tl in DataBase().get_new_tiles(self._uid, self._level_id).values():
+                self._notifications_panel2.add_notification('Открыт новый блок',
+                                                            'Доступен в редакторе', load_media(tl.IMAGE_NAME),
+                                                            duration=3)
+            completed_in = (datetime.now() - self._start_time).total_seconds()
+            if not self._best_time or completed_in < self._best_time:
+                self._best_time = completed_in
+            if self._uid != -1:
+                DataBase().save_completion(self._level_id, self._uid, completed_in)
+                post_event(UserEvents.LEVEL_COMPLETED, level_id=self._level_id, time=completed_in)
+
+        if hero.dead or hero.finished:
+            self.field_to_initial()
+            self.restart()
